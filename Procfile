77c5546749fffd0355e461eadf93ddbe98078b1d
ernandorodrigues <ernandorodrigues.ce@gmail.com>
2024-05-29 09:23:03 -0300

    novos arquivos heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn django_employee_qr.wsgi --log-file -
--- django_employee_qr/settings.py
+++ django_employee_qr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j7xv%)66l8u0^8sxnj7()z$zll8&a2*_h2r84yeo9f2fidx8^7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
