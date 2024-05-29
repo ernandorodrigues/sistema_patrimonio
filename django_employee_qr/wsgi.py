41ac3b8d3de11a30651c04c8b5720d177c4416eb
ernandorodrigues <ernandorodrigues.ce@gmail.com>
2024-05-29 15:19:24 -0300

    Corrigir erro de sintaxe no arquivo wsgi.py

--- django_employee_qr/wsgi.py
+++ django_employee_qr/wsgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-git add
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_employee_qr.settings')
 
 application = get_wsgi_application()
