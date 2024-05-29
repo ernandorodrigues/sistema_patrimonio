1d388fec910816825585a38e967b11a751ab51e4
ernandorodrigues <ernandorodrigues.ce@gmail.com>
2024-05-29 15:05:25 -0300

    Set STATIC_ROOT in settings.py

--- django_employee_qr/settings.py
+++ django_employee_qr/settings.py
@@ -132,8 +132,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'))
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login'
